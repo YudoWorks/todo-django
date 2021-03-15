d78f2166af385334d40a04f6fd35eae17ed255e0
Yudo Kartiko <yudobinus@gmail.com>
2021-03-15 23:17:19 +0700

    Add list, detail, create and delete urls with views and templates

--- todos/urls.py
+++ todos/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'todos'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>', views.DetailView.as_view(), name='detail'),
+    path('<int:todo_id>/delete', views.delete, name='delete'),
+    path('create', views.create, name='create')
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,31 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+
+from .models import Todo
 
 
-def index(request):
-    return HttpResponse('Todos Apps....')
+class IndexView(generic.ListView):
+    model = Todo
+    template_name = 'todos/index.html'
+
+
+class DetailView(generic.DetailView):
+    model = Todo
+    template_name = 'todos/detail.html'
+
+
+def delete(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    todo.delete()
+
+    return HttpResponseRedirect(reverse('todos:index'))
+
+
+def create(request):
+    todo = Todo(todo_text=request.POST["text"])
+    todo.save()
+
+    return HttpResponseRedirect(reverse('todos:index'))
