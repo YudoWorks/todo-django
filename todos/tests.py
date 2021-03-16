15b2556289cbbade759809afe759da0a601bee23
Yudo Kartiko <yudobinus@gmail.com>
2021-03-16 18:07:00 +0700

    test for display todo

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse
+from .models import Todo
+
+
+class TodoIndexViewTest(TestCase):
+    def test_todo(self):
+        Todo.objects.create(todo_text="Todo 1")
+        response = self.client.get(reverse('todos:index'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['todo_list'], ['<Todo: Todo 1>'])
